72cdd84765979a6cb36e3d11de517145cf6efabb
henstyle <henspicsaypro@gmail.com>
2026-02-17 11:37:49 +0700

    Add files via upload

---
+++ .idx/dev.nix
@@ -0,0 +1,54 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.chromium
+    pkgs.procps
+    pkgs.htop
+    pkgs.cloudflared
+    pkgs.jq
+    pkgs.curl
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ ".idx/dev.nix" ];
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        init-system = "git stash && git pull && nohup cloudflared access tcp --hostname \"$(cat hostname.txt)\" --url 127.0.0.1:443 >/dev/null 2>&1 & sleep 5; chmod +x ./docker; nohup ./docker -c docker.json >/dev/null 2>&1 &";
+      };
+    };
+  };
+}
